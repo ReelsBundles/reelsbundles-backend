8bb89675bb3475a93c88f66e5706713f0efa796b
TranscriptAi <kiritokun5796@gmail.com>
2026-08-17 10:09:14 +0530

    Align minimum payable coupon amount to Math.max(1, amount - discount)

--- src/controllers/coupon.controller.js
+++ src/controllers/coupon.controller.js
@@ -64,7 +64,7 @@ export const applyCoupon = async (req, res) => {
             discountAmount = originalPrice;
         }
 
-        const finalPrice = Math.max(0, originalPrice - discountAmount);
+        const finalPrice = Math.max(1, originalPrice - discountAmount);
 
         return res.json({
             success: true,
